71f6993bed17c520373ed2cea0cd56539aeda003
Yudha Permana <58971119+yudhapermana-yt@users.noreply.github.com>
2023-06-27 12:42:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt-get -y install vnstat php-cli
 mkdir /usr/local/vnstat-php -p
 cd /usr/local/vnstat-php || exit
 
-wget https://raw.githubusercontent.com/Athlon1600/vnstat-php/master/vnstat.php -O vnstat.php
+wget https://raw.githubusercontent.com/yudhapermana-yt/vnstat-php/master/vnstat.php
 
 nohup php -S 0.0.0.0:60222 vnstat.php > phpd.log 2>&1 &
